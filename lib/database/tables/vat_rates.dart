3c89da4347bac5ecd33cbf7035ad19b63108c41b
Minh <69456300+minhcz@users.noreply.github.com>
2025-08-04 18:31:57 +0200

    Delete lib directory

--- lib/database/tables/vat_rates.dart
+++
@@ -1,6 +0,0 @@
-import 'package:drift/drift.dart';
-
-class VatRates extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  RealColumn get rate => real()(); // phần trăm, ví dụ: 10.0, 21.0
-}
